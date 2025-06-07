32120b146cfa300922bdd6f0b734b53fc7e5dc8a
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-06-07 21:52:51 +0100

    feat: add HeroAboutSection component to partner  page

--- src/app/a-propos/page.js
+++ src/app/a-propos/page.js
@@ -1,9 +1,11 @@
 import HeroAboutSection from "@/components/hero-about-section"
+import PartnersSection from "@/components/partners-section"
 
 export default function Home() {
   return (
     <>
 <HeroAboutSection/>
+<PartnersSection/>
 
     </>
   )
