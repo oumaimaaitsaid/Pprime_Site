062f1ebac7c5338ccec845ef3213ccc27dcb2f05
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-06-07 21:53:22 +0100

    feat: add HeroAboutSection component to LeadershipSection

--- src/app/a-propos/page.js
+++ src/app/a-propos/page.js
@@ -1,11 +1,13 @@
 import HeroAboutSection from "@/components/hero-about-section"
 import PartnersSection from "@/components/partners-section"
+import LeadershipSection from "@/components/leadership-section"
 
 export default function Home() {
   return (
     <>
 <HeroAboutSection/>
 <PartnersSection/>
+<LeadershipSection/>
 
     </>
   )
