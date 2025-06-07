aaff5a59942711d618888ab93b7cf1acae232a1a
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-06-07 22:08:34 +0100

    feat: création de la section Leadership avec image et structure de mise en page

---
+++ src/components/leadership-section.js
@@ -0,0 +1,16 @@
+
+
+  return (
+    <section id="leadership-section" className="flex min-h-[500px] lg:min-h-[800px]">
+      {/* Photo à gauche - 50% */}
+      <div className="w-1/2 relative">
+        <Image src="/images/leadership/ceo-photo.jpg" alt="Dirigeant Pprime" fill className="object-cover" priority />
+      </div>
+
+      {/* Contenu à droite - 50% */}
+      <div className="w-1/2 bg-[#042433] text-white px-8 py-12 lg:px-16 lg:py-16 flex flex-col justify-center">
+       
+      </div>
+    </section>
+  )
+}
