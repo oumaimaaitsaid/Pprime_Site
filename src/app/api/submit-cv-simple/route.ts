3641144bcb43bd08215fcf783b9c43e7df1a1c4b
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-06-08 01:23:57 +0100

    modified

--- src/app/api/submit-cv-simple/route.ts
+++ src/app/api/submit-cv-simple/route.ts
@@ -1,5 +1,5 @@
 import { type NextRequest, NextResponse } from "next/server"
-import nodemailer from "nodemailer"
+import nodemailer from  "nodemailer"
 
 export async function POST(request: NextRequest) {
   try {
