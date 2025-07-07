f1df128e36f10ef194c06c8c28de54f3bfa88e59
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-07-07 21:53:37 +0100

    ✨ Ajout de la page secteurs d'activité
    - Intègre les composants HeroSection, PprimeSolutions1, PartnersSection, AboutSection et TestimonialsSection
    - Structure la page dans un <main> propre

---
+++ src/app/secteurs-activite/page.js
@@ -0,0 +1,18 @@
+import HeroSection from "../../components/hero-secteurs-section"
+import PprimeSolutions1 from "../../components/pprime-solutions1"   
+import PartnersSection from "../../components/partners-section"
+import AboutSection from "../../components/about-section"
+import TestimonialsSection from "../../components/testimonials-section"
+
+
+export default function SectorsActivite() {
+  return (
+    <main>
+      <HeroSection />
+      <PprimeSolutions1/>
+      <PartnersSection/>
+      <AboutSection />
+      <TestimonialsSection/>
+    </main>
+  )
+}
